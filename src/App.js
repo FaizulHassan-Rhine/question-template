e396f21492a974be894c24890effc045d5c45e12
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-07-04 13:55:13 +0600

    add new component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AnswerToQuestion from './Component/AnswerToQuestion/AnswerToQuestion';
 import UserForm from './Component/UserForm/UserForm';
+import SubjectForm from './Component/SubjectForm/SubjectForm';
 
 function App() {
   return (
-    <div className="App min-h-screen bg-yellow-300">
+    <div className="App min-h-screen bg-yellow-300 ">
       {/* <AnswerToQuestion /> */}
       <UserForm></UserForm>
+      {/* <SubjectForm></SubjectForm> */}
     </div>
   );
 }
