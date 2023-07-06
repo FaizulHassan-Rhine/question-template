6c67f4c811369291af84c5063bc18def5f96abd4
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2023-07-06 14:41:38 +0600

    add new components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import QuestionSubmit from './Component/Dashboard/QuestionSubmit/QuestionSubmit'
 import { Route, Routes } from 'react-router-dom';
 import { createContext, useState } from 'react';
 import Login from './Component/Dashboard/Login/Login';
+import Dashboard from './Component/Dashboard/Dashboard';
+import AllQuestionList from './Component/Dashboard/AllQuestionList/AllQuestionList';
+import ExamineeList from './Component/Dashboard/ExaminneList/ExamineeList';
+import ExamineeExamDetails from './Component/Dashboard/ExamineeExamDetails/ExamineeExamDetails';
 export const RegFormContextManager = createContext();
 
 function App() {
@@ -34,9 +38,13 @@ function App() {
           <Route path="/" element={<UserForm />} />
           <Route path="/subject" element={<SubjectForm />} />
           <Route path="/answertoquestion" element={<AnswerToQuestion />} />
-          <Route path="/login" element={<Login/>} />          
-          <Route path="/setsubject" element={<SetSubject/>} />          
-          <Route path="/createquestion" element={<QuestionSubmit/>} />          
+          <Route path="/login" element={<Login />} />
+          <Route path="/setsubject" element={<SetSubject />} />
+          <Route path="/createquestion" element={<QuestionSubmit />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/all-question-list" element={<AllQuestionList />} />
+          <Route path="/examinee-list" element={<ExamineeList />} />
+          <Route path="/examinee-exam-details" element={<ExamineeExamDetails />} />
         </Routes>
       </RegFormContextManager.Provider>
     </div>
---
+++ src/Component/Dashboard/Dashboard.js
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    const [open, setOpen] = useState(false);
+    const [open2, setOpen2] = useState(false);
+
+    const toggleMenu = () => {
+        setOpen(!open);
+    };
+    const toggleMenu2 = () => {
+        setOpen2(!open2);
+    };
+
+    const handleLogout = () => {
+
+        console.log("Logged out");
+    };
+
+    // const [seconds, setSeconds] = useState(60);
+
+    // useEffect(() => {
+    //     const countdown = setInterval(() => {
+    //         setSeconds(prevSeconds => prevSeconds - 1);
+    //     }, 1000);
+
+    //     if (seconds === 0) {
+    //         clearInterval(countdown);
+    //     }
+
+    //     return () => {
+    //         clearInterval(countdown);
+    //     };
+    // }, [seconds]);
+
+
+
+    return (
+        <div className="md:flex flex-col md:flex-row md:min-h-screen w-full">
+            <div
+
+                className="flex flex-col w-full md:w-64 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0"
+                x-data="{ open: false }"
+            >
+                <div className="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
+                    <Link
+                        href="#"
+                        className="text-2xl  font-bold uppercase"
+                    >
+                        Dashboard
+                    </Link>
+
+                    <button
+                        className="rounded-lg md:hidden  focus:outline-none focus:shadow-outline"
+                        onClick={toggleMenu}
+                    >
+                        <svg
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                x-show="!open"
+                                fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                                clipRule="evenodd"
+                            ></path>
+                            <path
+                                x-show="open"
+                                fillRule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+                <nav
+                    className={`flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto ${open ? 'block' : 'hidden'
+                        }`}
+                >
+                    <Link
+                        className="block px-4 py-2 mt-2 text-left text-sm font-semibold text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+
+                    >
+                        Admin Profile
+                    </Link>
+
+                    <div onClick={() => setOpen(!open)} className="relative z-[999]">
+                        <button
+                            // onClick={toggleMenu}
+                            className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-center bg-transparent rounded-lg  hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        >
+                            <span>Examinee Question</span>
+                            <svg
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                className={`inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform  ${open ? 'rotate-180' : 'rotate-0'
+                                    }`}
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </button>
+                        <div
+                            className={`${open ? 'block' : 'hidden'
+                                } absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg`}
+                        >
+                            <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800 ">
+                                <Link
+                                    className="block px-4 py-2 mt-2 text-sm font-semibold  text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+
+                                >
+                                    All Question List
+                                </Link>
+                                <Link
+                                    className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+
+                                >
+                                    Question Subject
+                                </Link>
+                                <Link
+                                    className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                >
+                                    Add New Question
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div onClick={() => setOpen2(!open2)} className="relative">
+                        <button
+                            // onClick={toggleMenu2}
+                            className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-center bg-transparent rounded-lg hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        >
+                            <span>Examinee Result</span>
+                            <svg
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                className={`inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform  ${open2 ? 'rotate-180' : 'rotate-0'
+                                    }`}
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </button>
+                        <div
+                            className={`${open2 ? 'block' : 'hidden'
+                                } absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg`}
+                        >
+                            <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                                <Link
+                                    className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+
+                                >
+                                    All Examinee List
+                                </Link>
+                                <Link
+                                    className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg  focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                >
+                                    Examinee Result
+                                </Link>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-5 text-left ml-3 ">
+                        <button
+                            onClick={handleLogout}
+                            className="text-sm text-white bg-yellow-400 px-4 py-1 rounded-md font-semibold"
+                        >
+                            Sign Out
+                        </button>
+                    </div>
+
+                    {/* <div>
+                        {seconds === 0 ? (
+                            <p>Time Over!!!</p>
+                        ) : (
+                            <p>{seconds} seconds remaining</p>
+                        )}
+                    </div> */}
+
+
+                </nav>
+            </div >
+        </div >
+    );
+};
+
+export default Sidebar;
